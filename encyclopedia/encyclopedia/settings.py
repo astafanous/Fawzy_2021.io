b003efa66a76fd194bd40f62db10c738b1f58e5b
astafanous <astafanousf@yahoo.com>
2021-07-02 09:40:35 +0200

    third commit

--- encyclopedia/encyclopedia/settings.py
+++ encyclopedia/encyclopedia/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'wiki'
+    'wiki',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
